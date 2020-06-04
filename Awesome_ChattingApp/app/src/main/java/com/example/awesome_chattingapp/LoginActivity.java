bc97792afc2302efb2f981e2283096ff4d8ccaed
JJongmen <pjy90123@gmail.com>
2020-06-05 07:00:37 +0900

    nickname error & anonymously login

--- Awesome_ChattingApp/app/src/main/java/com/example/awesome_chattingapp/LoginActivity.java
+++ Awesome_ChattingApp/app/src/main/java/com/example/awesome_chattingapp/LoginActivity.java
@@ -1,28 +1,81 @@
 package com.example.awesome_chattingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    EditText etNickname;
+    String nickname;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+
         Button enterButton = (Button)findViewById(R.id.enter_button);
         enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(LoginActivity.this,"Enter",Toast.LENGTH_SHORT).show();
-                Intent login2chat = new Intent(LoginActivity.this, ChatActivity.class);
-                startActivity(login2chat);
+                etNickname = (EditText)findViewById(R.id.nickname);
+                nickname = etNickname.getText().toString();
+                if(nickname.isEmpty()) {
+                    Toast.makeText(LoginActivity.this,"Please enter your nickname",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInAnonymously()
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInAnonymously:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+//                                    updateUI(user);
+                                    Toast.makeText(LoginActivity.this,"Nickname: " + nickname,Toast.LENGTH_SHORT).show();
+                                    Intent login2chat = new Intent(LoginActivity.this, ChatActivity.class);
+                                    startActivity(login2chat);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInAnonymously:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+//                                    updateUI(null);
+                                }
+
+                                // ...
+                            }
+                        });
             }
         });
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+    }
+
 }
