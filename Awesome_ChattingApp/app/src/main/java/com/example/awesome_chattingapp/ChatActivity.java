c154a61099f27e6174f683579af964b577b2666a
namHG <onep5286@gmail.com>
2020-06-06 00:36:58 +0900

    if empty message, no action

--- Awesome_ChattingApp/app/src/main/java/com/example/awesome_chattingapp/ChatActivity.java
+++ Awesome_ChattingApp/app/src/main/java/com/example/awesome_chattingapp/ChatActivity.java
@@ -125,6 +125,9 @@ public class ChatActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String sendText = chatEditText.getText().toString();
+                if(sendText.equals("")) {
+                    return;
+                }
                 // Write a message to the database
 
                 long now = System.currentTimeMillis();
