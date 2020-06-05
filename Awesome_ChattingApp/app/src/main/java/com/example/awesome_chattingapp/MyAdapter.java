a1e16eff905eee0734356824fe5455556c21ac51
JJongmen <pjy90123@gmail.com>
2020-06-05 09:55:11 +0900

    Message data read

--- Awesome_ChattingApp/app/src/main/java/com/example/awesome_chattingapp/MyAdapter.java
+++ Awesome_ChattingApp/app/src/main/java/com/example/awesome_chattingapp/MyAdapter.java
@@ -7,8 +7,10 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private String[] mDataset;
+    private ArrayList<Chat> mDataset;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -23,7 +25,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public MyAdapter(String[] myDataset) {
+    public MyAdapter(ArrayList<Chat> myDataset) {
         mDataset = myDataset;
     }
 
@@ -44,13 +46,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.setText(mDataset[position]);
+        holder.textView.setText(mDataset.get(position).getNickname() + " : " + mDataset.get(position).getMessage());
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mDataset.size();
     }
 }
